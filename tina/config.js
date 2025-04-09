886251f8f138cdc724b38181d841b03e83fd0b32
James <james@mcmk.in>
2025-04-09 17:11:03 +0100

    Update theme

---
+++ tina/config.js
@@ -0,0 +1,108 @@
+import { defineConfig } from "tinacms";
+import { blog_postFields } from "./templates";
+import { raw_fileFields } from "./templates";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "39d54616-62e3-4fb7-9d3b-677e0ebb1ca1", // Get this from tina.io
+  token: "a6a5bf508f29f2b631a981770fa8f13400d665d0", // Get this from tina.io
+  client: { skip: true },
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "static",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        format: "md",
+        label: "Pages",
+        name: "pages",
+        path: "content",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "**/*",
+          exclude: "posts/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+          ...blog_postFields(),
+        ],
+      },
+      {
+        format: "md",
+        label: "Posts",
+        name: "posts",
+        path: "content/posts",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "**/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+          ...blog_postFields(),
+        ],
+      },
+      {
+        format: "md",
+        label: "CSS",
+        name: "css",
+        path: "assets/css",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "**/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+          ...raw_fileFields(),
+        ],
+      },
+      {
+        format: "md",
+        label: "Javascript",
+        name: "javascript",
+        path: "assets/js",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "**/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+          ...raw_fileFields(),
+        ],
+      },
+    ],
+  },
+});
