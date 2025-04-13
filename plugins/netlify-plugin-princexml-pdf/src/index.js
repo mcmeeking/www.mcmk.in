7b07d8935f2ad290174cd747f6b920b300172341
James <james@mcmk.in>
2025-04-13 17:21:29 +0100

    Update plugin

--- plugins/netlify-plugin-princexml-pdf/src/index.js
+++ plugins/netlify-plugin-princexml-pdf/src/index.js
@@ -1,28 +1,34 @@
 /* eslint-disable no-unused-vars */
 
-const fs = require("fs");
-const Prince = require("prince");
+import Prince from "prince";
+import fs from "fs";
 
-module.exports = {
-  async onPostBuild({
-    inputs,
-    constants: { PUBLISH_DIR },
-    utils: { build, status },
-  }) {
-    console.log("Gnerating PDF with PrinceXML...");
-    console.log("Inputs: ", inputs);
-    console.log("Publish Directory: ", PUBLISH_DIR);
-    await Prince()
-      .inputs(inputs.url)
-      .output(PUBLISH_DIR + "/" + inputs.fileName)
-      .execute()
-      .then(
-        function () {
-          status.show({ summary: "PDF generated successfully!" });
-        },
-        function (error) {
-          build.failBuild("Error: Failed to generate PDF:", { error });
-        },
-      );
-  },
-};
+export async function onPostBuild({
+  inputs, constants: { PUBLISH_DIR }, utils: { build, status },
+}) {
+  let licenseFile;
+  let text = '';
+
+  // Create license file if we have a license key in the env vars, otherwise use the default
+  if (process.env.PRINCE_LICENSE) {
+    licenseFile = "license.dat";
+    fs.writeFileSync(licenseFile, process.env.PRINCE_LICENSE);
+  } else {
+    text = "Prince license not found, please set the 'PRINCE_LICENSE' environment variable to remove the watermark.",
+    licenseFile = "license/license.dat";
+  }
+
+  await Prince()
+    .inputs(inputs.url)
+    .output(PUBLISH_DIR + "/" + inputs.fileName)
+    .license(licenseFile)
+    .execute()
+    .then(
+      function () {
+        status.show({ summary: `PDF generated successfully to ${PUBLISH_DIR}/${inputs.fileName}`, text: text });
+      },
+      function (error) {
+        build.failBuild(error.error);
+      }
+    );
+}
